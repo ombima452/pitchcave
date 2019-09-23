04341dbc2290b536f06188651d376bd07834dcee
ombima452 <christineombima452@gmail>
2019-09-23 09:05:43 +0300

    create pitch tables

--- app/models.py
+++ app/models.py
@@ -40,29 +40,29 @@ class User(UserMixin,db.Model):
 
 
 
-# class Pitch(db.Model):
+class Pitch(db.Model):
  
-#     __tablename__ = 'pitches'
+    __tablename__ = 'pitches'
 
-#     id = db.Column(db.Integer, primary_key = True)
-#     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
-#     description = db.Column(db.String(), index = True)
-#     title = db.Column(db.String())
-#     # downvotes = db.Column(db.Integer, default=int(0))
-#     # upvotes = db.Column(db.Integer, default=int(0))
-#     category = db.Column(db.String(255), nullable=False)
-#     comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
-#     upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
-#     downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    description = db.Column(db.String(), index = True)
+    title = db.Column(db.String())
+    # downvotes = db.Column(db.Integer, default=int(0))
+    # upvotes = db.Column(db.Integer, default=int(0))
+    category = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
 
     
-#     @classmethod
-#     def get_pitches(cls, id):
-#         pitches = Pitch.query.order_by(pitch_id=id).desc().all()
-#         return pitches
+    @classmethod
+    def get_pitches(cls, id):
+        pitches = Pitch.query.order_by(pitch_id=id).desc().all()
+        return pitches
 
-#     def __repr__(self):
-#         return f'Pitch {self.description}'
+    def __repr__(self):
+        return f'Pitch {self.description}'
 
     
 
